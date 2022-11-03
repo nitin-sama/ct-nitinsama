1fe22e045a56d94acebfce7202917777867f98c5
Nitin Thakur <97695348+nitin-sama@users.noreply.github.com>
2022-11-03 15:00:08 +0530

    Adding QA accordian!

---
+++ self-projects/accordian-qa/main.js
@@ -0,0 +1,22 @@
+// Accordian
+
+jQuery(document).ready(function ($) {
+  var $titleTab = $('.title_tab');
+  $('.Accordion_item:eq(0)').find('.title_tab').addClass('active').next().stop().slideDown(300);
+  $('.Accordion_item:eq(0)').find('.inner_content').find('p').addClass('show');
+  $titleTab.on('click', function(e) {
+  e.preventDefault();
+      if ( $(this).hasClass('active') ) {
+          $(this).removeClass('active');
+          $(this).next().stop().slideUp(500);
+          $(this).next().find('p').removeClass('show');
+      } else {
+          $(this).addClass('active');
+          $(this).next().stop().slideDown(500);
+          $(this).parent().siblings().children('.title_tab').removeClass('active');
+          $(this).parent().siblings().children('.inner_content').slideUp(500);
+          $(this).parent().siblings().children('.inner_content').find('p').removeClass('show');
+          $(this).next().find('p').addClass('show');
+      }
+  });
+});
